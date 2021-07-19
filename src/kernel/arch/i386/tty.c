269789863caed846c7f776d4ac0d63a0ad5aa6c9
dzwdz <kg67199@gmail.com>
2021-07-19 18:30:40 +0200

    fix some style inconsistenties

--- src/kernel/arch/i386/tty.c
+++ src/kernel/arch/i386/tty.c
@@ -10,23 +10,20 @@ static struct vga_cell *vga     = (void*) 0xB8000;
 static size_t           vga_pos = 0;
 
 static void tty_scroll() {
-	for (size_t i = 0; i < vga_len - 80; i++) {
+	for (size_t i = 0; i < vga_len - 80; i++)
 		vga[i] = vga[i + 80];
-	}
 	vga_pos -= 80;
 }
 
-void tty_putchar(char c)
-{
-	if (vga_pos >= vga_len - 80) tty_scroll();
+void tty_putchar(char c) {
+	if (vga_pos >= vga_len - 80)
+		tty_scroll();
 	vga[vga_pos++].c = c;
 }
 
-void tty_write(const char *buf, size_t len)
-{
-	for (size_t i = 0; i < len; i++) {
+void tty_write(const char *buf, size_t len) {
+	for (size_t i = 0; i < len; i++)
 		tty_putchar(buf[i]);
-	}
 }
 
 void tty_clear() {
