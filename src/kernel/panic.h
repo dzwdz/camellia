b2e43dd96f385e275c08ad531cd1c6bb65e2011f
dzwdz <kg67199@gmail.com>
2021-08-03 17:38:12 +0200

    put the NUM2STR macro in util.h
    i'll use it outside of panic.h

--- src/kernel/panic.h
+++ src/kernel/panic.h
@@ -1,13 +1,10 @@
 #pragma once
 #include <kernel/arch/generic.h>
-
-// dumb c shit
-#define panic_tostr2(x) #x
-#define panic_tostr(x)  panic_tostr2(x)
+#include <kernel/util.h>
 
 #define panic() do { \
 	log_const(" PANIC! at the "); \
 	log_const(__func__); \
-	log_const(" (" __FILE__ ":" panic_tostr(__LINE__) ") "); \
+	log_const(" (" __FILE__ ":" NUM2STR(__LINE__) ") "); \
 	halt_cpu(); \
 } while (0)
--- src/kernel/util.h
+++ src/kernel/util.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <stddef.h>
 
+#define __NUM2STR(x) #x
+#define NUM2STR(x)  __NUM2STR(x)
+
 void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void *s, int c, size_t n);
