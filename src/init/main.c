4e29459eecf86b1751ec6f4aaa28e4c0f20775d3
dzwdz <kg67199@gmail.com>
2021-08-12 18:02:32 +0000

    a mess that shouldn't land in main

--- src/init/main.c
+++ src/init/main.c
@@ -1,15 +1,47 @@
 #include <kernel/syscalls.h>
+#include <stdint.h>
 
-#define STR_64  "This string has exactly 64 characters. It'll be repeated a bunch"
-#define STR_256 STR_64  STR_64  STR_64  STR_64
-#define STR_1K  STR_256 STR_256 STR_256 STR_256
-#define STR_4K  STR_1K  STR_1K  STR_1K  STR_1K
-#define STR_LNG "start " STR_4K STR_4K " finished! "
+volatile char multi_buf[0x2000] __attribute__((section("text"))) = {0};
+
+inline void _debuglog_hex(const char *buf, size_t len) {
+	char hex[2];
+	for (size_t i = 0; i < len; i++) {
+		hex[0] = (buf[i] & 0xF0) >> 4;
+		hex[0] += '0';
+		if (hex[0] > '9')
+			hex[0] += 'a' - '9' - 1;
+
+		hex[1] = buf[i] & 0xF;
+		hex[1] += '0';
+		if (hex[1] > '9')
+			hex[1] += 'a' - '9' - 1;
+		
+		_syscall_debuglog(hex, 2);
+	}
+}
+#define _debuglog_var(var) _debuglog_hex((void*)&var, sizeof(var))
+
+// takes a cstring and copies it right before a page boundary
+const char *multipageify(const char *str) {
+	char *to = ((uintptr_t)multi_buf & ~0xFFF) + 0xFFF;
+	int asdf = 0x12345678;
+	_debuglog_hex((void*)&asdf, 4);
+	_syscall_debuglog(" a2s ", 5);
+	do {
+		*to++ = *str;
+		_syscall_debuglog("!", 1);
+	} while (*str++ != '\0');
+	_syscall_debuglog(" a3s ", 5);
+	return to;
+}
 
 int main() {
-	// try to print a string which is longer than a page
-	_syscall_debuglog(STR_LNG,
-	           sizeof(STR_LNG) - 1);
+	// try to print a string crossing page boundaries
+	_syscall_debuglog(" a1s ", 5);
+	_syscall_debuglog(
+		multipageify("I cross pages"),
+		      sizeof("I cross pages") - 1);
+	_syscall_debuglog(" a4s ", 5);
 
 	_syscall_exit("bye from init! ",
 	       sizeof("bye from init! ") - 1);
