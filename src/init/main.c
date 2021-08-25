7e6034c601aa04f27eaa605fa2639804eeca8280
dzwdz <kg67199@gmail.com>
2021-08-25 15:50:43 +0200

    reorganise init/main.c, add some "tests"

--- src/init/main.c
+++ src/init/main.c
@@ -2,6 +2,40 @@
 #include <shared/syscalls.h>
 #include <stdint.h>
 
+#define argify(str) str, sizeof(str) - 1
+#define log(str) _syscall_fd_write(tty_fd, argify(str))
+
+__attribute__((section("text")))
+int tty_fd;
+
+void misc_tests(void);
+// takes a cstring and copies it right before a page boundary
+const char *multipageify(const char *str);
+
+#define mount(fd, path) _syscall_fd_mount(fd, path, sizeof(path)-1)
+
+int main(void) {
+	// TODO don't dispatch /ttywhatever to the tty driver
+
+	tty_fd = _syscall_fs_open("/tty", sizeof("/tty") - 1);
+	if (tty_fd < 0)
+		_syscall_exit(argify("couldn't open tty"));
+	log(" opened /tty ");
+
+	misc_tests();
+
+	_syscall_exit(argify("my job here is done."));
+}
+
+void misc_tests(void) {
+	int res;
+	res = _syscall_fs_open(argify("/tty/nonexistant"));
+	if (res >= 0) log("test failed ");
+	res = _syscall_fs_open(argify("/ttynonexistant"));
+	if (res >= 0) log("test failed ");
+	log("the \"tests\" went ok");
+}
+
 // takes a cstring and copies it right before a page boundary
 const char *multipageify(const char *str) {
 	static char buf[0x2000] __attribute__((section("text")));
@@ -12,34 +46,4 @@ const char *multipageify(const char *str) {
 		*cur++ = *str;
 	} while (*str++ != '\0');
 	return out;
-}
-
-#define mount(fd, path) _syscall_fd_mount(fd, path, sizeof(path)-1)
-
-int main(void) {
-	int fd;
-
-	mount(0, "/some/where");
-	mount(0, "/some");
-
-	_syscall_fs_open(  // should fail
-			multipageify("/some/where/file"),
-			      sizeof("/some/where/file") - 1);
-
-	fd = _syscall_fs_open(
-			multipageify("/tty"),
-			      sizeof("/tty") - 1);
-	// TODO don't dispatch /ttywhatever to the tty driver
-
-	if (fd < 0) {
-		_syscall_exit("couldn't open tty",
-		       sizeof("couldn't open tty") - 1);
-	}
-
-	_syscall_fd_write(fd, "fd test ", 8);
-	_syscall_fd_close(fd);
-	_syscall_fd_write(fd, "fd test ", 8); // should fail
-
-	_syscall_exit("bye from init! ",
-	       sizeof("bye from init! ") - 1);
 }
