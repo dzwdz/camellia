12ac8f2134d0ca0b103e70515acad3d6ccf9b9c0
dzwdz <kg67199@gmail.com>
2021-10-07 06:37:07 +0000

    kernel/i386: add 16bit port io functions

--- src/kernel/arch/i386/port_io.h
+++ src/kernel/arch/i386/port_io.h
@@ -4,9 +4,19 @@ static inline void port_out8(uint16_t port, uint8_t val) {
 	asm volatile("outb %0, %1" : : "a" (val), "Nd" (port));
 }
 
+static inline void port_out16(uint16_t port, uint16_t val) {
+	asm volatile("outw %0, %1" : : "a" (val), "Nd" (port));
+}
+
 static inline uint8_t port_in8(uint16_t port) {
 	uint8_t val;
 	asm volatile("inb %1, %0" : "=a" (val) : "Nd" (port));
+	return val;
+}
+
+static inline uint8_t port_in16(uint16_t port) {
+	uint16_t val;
+	asm volatile("inw %1, %0" : "=a" (val) : "Nd" (port));
 	return val;
 }
 
