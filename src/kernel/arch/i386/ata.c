0c000a03560eeab06154c1fb80b7a59a5c0f9ac7
dzwdz <kg67199@gmail.com>
2021-10-08 13:06:42 +0000

    ata: proper drive type detection; soft reset; 400ns delay function

--- src/kernel/arch/i386/ata.c
+++ src/kernel/arch/i386/ata.c
@@ -20,6 +20,10 @@ enum {
 	DRV    = 6,
 	CMD    = 7,
 	STATUS = 7,
+
+	/* note: the OSDev wiki uses a different base port for the control port
+	 *       however i can just use this offset and stuff will just work tm */
+	CTRL   = 0x206,
 }; // offsets
 
 // get I/O port base for drive
@@ -28,12 +32,50 @@ static uint16_t ata_iobase(int drive) {
 	return secondary ? 0x170 : 0x1F0;
 }
 
+static void ata_400ns(void) {
+	uint16_t base = ata_iobase(0); // doesn't matter
+	for (int i = 0; i < 4; i++)
+		port_in8(base + STATUS);
+}
+
 static void ata_driveselect(int drive, int block) {
 	uint8_t v = 0xE0;
 	if (drive&1) // slave?
 		v |= 0x10; // set drive number bit
 	// TODO account for block
 	port_out8(ata_iobase(drive) + DRV, v);
+}
+
+static void ata_softreset(int drive) {
+	uint16_t iobase = ata_iobase(drive);
+	port_out8(iobase + CTRL, 4);
+	port_out8(iobase + CTRL, 0);
+	ata_400ns();
+
+	uint16_t timeout = 10000;
+	while (--timeout) { // TODO separate polling function
+		uint8_t v = port_in8(iobase + STATUS);
+		if (v & 0x80) continue; // still BSY, continue
+		if (v & 0x40) break; // RDY, break
+		// TODO check for ERR
+	}
+}
+
+static void ata_detecttype(int drive) {
+	ata_softreset(drive);
+	ata_driveselect(drive, 0);
+	ata_400ns();
+	switch (port_in8(ata_iobase(drive) + LBAmid)) {
+		case 0:
+			ata_drives[drive].type = DEV_PATA;
+			break;
+		case 0x14:
+			ata_drives[drive].type = DEV_PATAPI;
+			return true;
+		default:
+			ata_drives[drive].type = DEV_UNKNOWN;
+			return false;
+	}
 }
 
 static bool ata_identify(int drive) {
@@ -52,19 +94,6 @@ static bool ata_identify(int drive) {
 	if (v == 0) return false; // nonexistent drive
 	while (port_in8(iobase + STATUS) & 0x80);
 
-	/* detect device type */
-	switch (port_in8(iobase + LBAmid)) {
-		case 0:
-			ata_drives[drive].type = DEV_PATA;
-			break;
-		case 0x14:
-			ata_drives[drive].type = DEV_PATAPI;
-			return true;
-		default:
-			ata_drives[drive].type = DEV_UNKNOWN;
-			return false;
-	}
-
 	/* pool until bit 3 (DRQ) or 0 (ERR) is set */
 	while (!((v = port_in8(iobase + STATUS) & 0x9)));
 	if (v & 1) return false; /* ERR was set, bail */
@@ -80,11 +109,13 @@ void ata_init(void) {
 	for (int i = 0; i < 4; i++) {
 		tty_const("probing drive ");
 		_tty_var(i);
-		if (ata_identify(i)) {
-			tty_const(" - ");
-			_tty_var(ata_drives[i].sectors);
-			tty_const(" sectors (512b)");
-		}
+		ata_detecttype(i);
+		_tty_var(ata_drives[i].type);
+		// if (ata_identify(i)) {
+		// 	tty_const(" - ");
+		// 	_tty_var(ata_drives[i].sectors);
+		// 	tty_const(" sectors (512b)");
+		// }
 		tty_const("\n");
 	}
 }
