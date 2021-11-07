90cb50f15ee6e4016fcfc6345668046c68ae1f3d
dzwdz <kg67199@gmail.com>
2021-11-07 18:43:47 +0100

    init: add strcpy/memcpy
    just copied straight over from the kernel's util.c

--- src/init/stdlib.c
+++ src/init/stdlib.c
@@ -5,6 +5,21 @@
 int __tty_fd;
 
 
+void *memcpy(void *dest, const void *src, size_t n) {
+	char *d = dest;
+	const char *s = src;
+	for (size_t i = 0; i < n; i++)
+		d[i] = s[i];
+	return dest;
+}
+
+void *memset(void *s, int c, size_t n) {
+	uint8_t *s2 = s;
+	for (size_t i = 0; i < n; n++)
+		s2[i] = c;
+	return s;
+}
+
 int memcmp(const void *s1, const void *s2, size_t n) {
 	const unsigned char *c1 = s1, *c2 = s2;
 	for (size_t i = 0; i < n; i++) {
--- src/init/stdlib.h
+++ src/init/stdlib.h
@@ -3,6 +3,8 @@
 
 extern int __tty_fd;
 
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *s, int c, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
 int strcmp(const char *s1, const char *s2);
 size_t strlen(const char *s);
