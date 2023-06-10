ff154205e1671e11ed8566740c1cefaad79c2512
dzwdz <kg67199@gmail.com>
2023-06-10 19:12:49 +0200

    user: fix floating point operations

--- src/user/lib/_start.s
+++ src/user/lib/_start.s
@@ -5,6 +5,12 @@
 _start:
 	mov %rsp, %rdi
 	and $~0xF, %rsp
+
+	/* prevent floating point crashes. thanks heat */
+	push $0x1f80
+	ldmxcsr (%rsp)
+	add $8, %rsp
+
 	call _start2
 	hlt
 	/* the call shouldn't return, thus the hlt.
